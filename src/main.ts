072b38aebe4b3c8089a3c1dec45c21768aa46fe4
Svygzhryr <pamixy@gmail.com>
2024-03-11 08:53:54 +0300

    fix: swagger docs from yaml

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,19 @@
-import {
-  DocumentBuilder,
-  SwaggerCustomOptions,
-  SwaggerModule,
-} from '@nestjs/swagger';
+import * as yaml from 'yaml';
+import { SwaggerModule } from '@nestjs/swagger';
+import * as path from 'path';
+import { readFileSync } from 'fs';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  const location = path.join(__dirname, '..', 'doc', 'api.yaml');
+  const file = readFileSync(location, { encoding: 'utf-8' });
+  const document = yaml.parse(file);
+
   const port = process.env.PORT || 4000;
   const app = await NestFactory.create(AppModule);
 
-  const config = new DocumentBuilder()
-    .setTitle('REST music service')
-    .setDescription('Home library api service description')
-    .setVersion('0.0.1')
-    .build();
-  const document = SwaggerModule.createDocument(app, config);
-
   SwaggerModule.setup('docs', app, document);
-
   await app.listen(port);
 }
 
